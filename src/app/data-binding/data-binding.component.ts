fdee06d88499832e322d3eb6291f2c176dd8ad8e
Vinicius Mendes <vinimcastro@hotmail.com>
2023-04-02 19:35:05 -0300

    Estudos Property Binding, Class Style e Event Binding

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.scss'],
+})
+export class DataBindingComponent implements OnInit {
+  public url: string = 'https://www.amazon.com.br/ref=nav_logo';
+  cursoAngular: boolean = true;
+  urlImage =
+    'https://cdn.lorem.space/images/movie/.cache/150x220/black-widow-2.jpg';
+
+  valorAtual: string = '';
+  valorSalvo = '';
+  isMouseOver: boolean = false;
+
+  salvarValor(valor: any) {
+    this.valorSalvo = valor;
+  }
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  botaoClicado() {
+    alert('Nu ze');
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
