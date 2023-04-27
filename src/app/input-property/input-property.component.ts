fbe5ed6e0080d1f983a6e0c040c4c3a6e8fcf493
Vinicius Mendes <vinimcastro@hotmail.com>
2023-04-27 18:28:03 -0300

    att data-binding

---
+++ src/app/input-property/input-property.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-property',
+  templateUrl: './input-property.component.html',
+  styleUrls: ['./input-property.component.scss']
+})
+export class InputPropertyComponent {
+
+}
